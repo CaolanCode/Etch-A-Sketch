f32330a26c3396a903f557e40bb9491cd5276bbd
CaolanCode <cmacmahon3@gmail.com>
2022-12-11 19:45:31 +0000

    commit listeners on buttons

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const container = document.getElementById('container');
-let counter = 1;
+const cleaerButton = document.getElementById('clearButton')
 
 function makeSquares(size){
   container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
@@ -7,16 +7,24 @@ function makeSquares(size){
   for(let i = 0; i < (size * size); i++){
     let div = document.createElement('div');
     div.classList.add('box');
-    div.innerText = counter;
     div.addEventListener("mouseover", changeColour);
     container.appendChild(div);
-    counter += 1;
   }
 }
 
 function changeColour(event){
   event.target.style.backgroundColor = 'black';
-
 }
 
-makeSquares(16);
+document.getElementById('startButton').addEventListener('click', getGridSize);
+document.getElementById('clearButton').addEventListener('click', getGridSize);
+
+
+function getGridSize(){
+  let size = parseInt(prompt("Enter the grid size, 1-100:"));
+  while(size > 100){
+    size = parseInt(prompt("Please enter a number below 100:"));
+  }
+  makeSquares(size);
+}
+
